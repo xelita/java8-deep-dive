df85423e26f99f61f61735ae8c8cae805544a6bc
xelita <admin@xelita.com>
2015-11-06 18:07:57 +0100

    study on Optional

--- src/test/java/com/xelita/study/java/OptionalTest.java
+++ src/test/java/com/xelita/study/java/OptionalTest.java
@@ -23,27 +23,42 @@ public class OptionalTest {
 	}
 
 	@Test
-	public void optionalFilter() throws Exception {
+	public void orElse() throws Exception {
+		Optional<Integer> val = Optional.empty();
+		assertThat(val.orElse(10), is(10));
+	}
+
+	@Test
+	public void orElseGet() throws Exception {
+		Optional<Integer> val = Optional.empty();
+		assertThat(val.orElseGet(() -> 10), is(10));
+	}
+
+	@Test(expected = IllegalStateException.class)
+	public void orElseThrow() throws Exception {
+		Optional<Integer> val = Optional.empty();
+		val.orElseThrow(IllegalStateException::new);
+	}
+
+	@Test
+	public void ifPresent() throws Exception {
+		List<Integer> values = new ArrayList<>();
+		Optional<Integer> val = Optional.of(10);
+		val.filter(s -> s < 100).ifPresent(values::add);
+		assertThat(values.size(), is(1));
+	}
+
+	@Test
+	public void filter() throws Exception {
 		Optional<Integer> val = Optional.of(10);
 		Optional<Integer> result = val.filter(s -> s > 10);
 		assertThat(result.isPresent(), is(false));
 	}
 
 	@Test
-	public void optionalOrElse() throws Exception {
-		Optional<Integer> val = Optional.empty();
-		assertThat(val.orElse(10), is(10));
-	}
-
-	@Test
-	public void optionalOrElseGet() throws Exception {
-		Optional<Integer> val = Optional.empty();
-		assertThat(val.orElseGet(() -> 10), is(10));
-	}
-
-	@Test(expected = IllegalStateException.class)
-	public void optionalOrElseThrow() throws Exception {
-		Optional<Integer> val = Optional.empty();
-		val.orElseThrow(IllegalStateException::new);
+	public void map() throws Exception {
+		Optional<Integer> val = Optional.of(10);
+		Optional<Integer> result = val.map(s -> s / 2);
+		assertThat(result.get(), is(5));
 	}
 }
